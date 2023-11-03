0ce93934c287931dba59f1a5cf8f9890069b6fc0
Luiggi Yantas <86317658+Luiggi-piero@users.noreply.github.com>
2023-11-03 15:44:26 -0500

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -4,4 +4,5 @@ SHOW GLOBAL STATUS LIKE 'Created_tmp_tables';
 
 SHOW GLOBAL VARIABLES LIKE 'tmp_table_size';
 
+-- Modifica la variable de ambiente tmp_table_size(representa la canidad de espacio disponible para almacenar tablas temporales en memoria)
 SET GLOBAL tmp_table_size = 117825792;
